3fa83df93c218b089f7b9fd6eb0b8a9e8ccf5f4f
h3nnn4n <uber.renan@gmail.com>
2023-06-02 07:24:01 -0300

    Set leg servos

--- src/CubotronUno/main.cpp
+++ src/CubotronUno/main.cpp
@@ -9,7 +9,6 @@
 #include <Servo.h>
 #include <Wire.h>
 
-const int     servoPin       = 9;
 const int     ledPin         = LED_BUILTIN;
 const int32_t interval       = 100;
 uint32_t      previousMillis = 0;
@@ -23,29 +22,26 @@ const int servo_update_delay = 10;
 const int servo_min_pulse_width = 500;
 const int servo_max_pulse_width = 2500;
 
-Servo servo;
+Servo servo_femur;
+Servo servo_tibia;
+Servo servo_coxa;
 
 void blinkenlights();
 
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
-    servo.attach(servoPin, servo_min_pulse_width, servo_max_pulse_width);
+
+    servo_femur.attach(9, servo_min_pulse_width, servo_max_pulse_width);
+    servo_tibia.attach(10, servo_min_pulse_width, servo_max_pulse_width);
+    servo_coxa.attach(11, servo_min_pulse_width, servo_max_pulse_width);
+
+    servo_femur.write(90);
+    servo_tibia.write(90);
+    servo_coxa.write(90);
 }
 
 void loop() {
     blinkenlights();
-
-    for (pos = min_angle; pos <= max_angle; pos += 1) {
-        servo.write(pos);
-        delay(servo_update_delay);
-    }
-    delay(500);
-
-    for (pos = max_angle; pos >= min_angle; pos -= 1) {
-        servo.write(pos);
-        delay(servo_update_delay);
-    }
-    delay(500);
 }
 
 void blinkenlights() {
