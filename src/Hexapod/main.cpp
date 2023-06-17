e1d9d7ee2e73d7590d285390a127cfb06b98c6a6
h3nnn4n <uber.renan@gmail.com>
2023-06-17 17:19:24 -0300

    Direct angle control + ik mode

--- src/Hexapod/main.cpp
+++ src/Hexapod/main.cpp
@@ -33,9 +33,16 @@ float feet_x = 0;
 float feet_y = 250;
 float feet_z = 0;
 
-bool button_down = false;
+float coxa_angle  = 0;
+float femur_angle = 0;
+float tibia_angle = 0;
 
-uint16_t val;
+bool button_down = false;
+bool ik_mode     = true;
+
+uint16_t val_x;
+uint16_t val_y;
+uint16_t val_z;
 
 void setup() {
     Serial.begin(115200);
@@ -54,7 +61,7 @@ void setup() {
 
     coxa.set_angle_range(-90.0f, 90.0f);
     femur.set_angle_range(-90.0f, 90.0f);
-    tibia.set_angle_range(-90.0f, 90.0f);
+    tibia.set_angle_range(0.0f, 180.0f);
 
     leg.enable_servos();
     leg.init();
@@ -65,29 +72,46 @@ void setup() {
 void loop() {
     blinkenlights.update();
 
-    val    = analogRead(A0);
-    feet_x = map(val, 0, 1023, -300, 300);
-
-    val    = analogRead(A1);
-    feet_y = map(val, 0, 1023, 0, 300);
-
-    val    = analogRead(A2);
-    feet_z = map(val, 0, 1023, -300, 300);
-
     button_down = digitalRead(2);
+    val_x       = analogRead(A0);
+    val_y       = analogRead(A1);
+    val_z       = analogRead(A2);
 
     Serial.print(button_down);
-    Serial.print(" ");
-    Serial.print(feet_x);
-    Serial.print(" ");
-    Serial.print(feet_y);
-    Serial.print(" ");
-    Serial.print(feet_z);
+
+    if (ik_mode) {
+        feet_x = map(val_x, 0, 1023, -300, 300);
+        feet_y = map(val_y, 0, 1023, 0, 300);
+        feet_z = map(val_z, 0, 1023, -300, 300);
+
+        Serial.print(" ");
+        Serial.print(feet_x);
+        Serial.print(" ");
+        Serial.print(feet_y);
+        Serial.print(" ");
+        Serial.print(feet_z);
+    } else {
+        coxa_angle  = map(val_z, 0, 1023, -90, 90);
+        femur_angle = map(val_x, 0, 1023, -90, 90);
+        tibia_angle = map(val_y, 0, 1023, 0, 180);
+
+        Serial.print(" ");
+        Serial.print(coxa_angle);
+        Serial.print(" ");
+        Serial.print(femur_angle);
+        Serial.print(" ");
+        Serial.print(tibia_angle);
+    }
+
     Serial.println();
 
     if (button_down) {
-        leg.move_feet_to(feet_x, feet_y, feet_z);
+        if (ik_mode) {
+            leg.move_feet_to(feet_x, feet_y, feet_z);
+        } else {
+            leg.move_joints(coxa_angle, femur_angle, tibia_angle);
+        }
     } else {
-        leg.move_feet_to(0, 250, 0);
+        leg.move_feet_to(0, 300, 0);
     }
 }
