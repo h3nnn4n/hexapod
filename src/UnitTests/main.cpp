41b83ecacc345caf8e436eb19441eb2632ad1b2b
h3nnn4n <git@renan.digital>
2023-06-16 21:22:19 -0300

    basic fk unit test

--- src/UnitTests/main.cpp
+++ src/UnitTests/main.cpp
@@ -12,16 +12,32 @@
 #include <Joint.h>
 #include <Leg.h>
 #include <PWMServoDriver.h>
+#include <Utils.h>
 #include <VectorDatatype.h>
 #include <Wire.h>
 
 #include <AUnit.h>
 
-test(foo) {
-    int x = 1;
-    assertEqual(x, 1);
+char buffer[100];
+
+Leg leg = Leg(nullptr, nullptr, nullptr);
+
+test(foo) { assertEqual(2 + 2, 4); }
+
+test(fk) {
+    vec3_t result = leg.forward_kinematics(0, 0, 0);
+
+    serial_println_vec3(result);
+
+    assertEqual(result.x, 0.0);
+    assertEqual(result.y, 300.0);
+    assertEqual(result.z, 0.0);
 }
 
-void setup() { Serial.begin(115200); }
+void setup() {
+    Serial.begin(115200);
+
+    leg.disable_servos();
+}
 
 void loop() { aunit::TestRunner::run(); }
