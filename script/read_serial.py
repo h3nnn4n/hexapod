64fbe534a38e7a983c88ce245bbbaf1b54e2a5ee
h3nnn4n <git@renan.digital>
2023-06-17 07:04:43 -0300

    scripts: Make read_serial configurable

--- script/read_serial.py
+++ script/read_serial.py
@@ -1,8 +1,17 @@
+#!/usr/bin/env python3
+
 import serial
+
+from decouple import config
+
+
+SERIAL_PORT = config("SERIAL_PORT", default="/dev/ttyUSB0")
+BAUD_RATE = config("BAUD_RATE", default=115200)
+SERIAL_TIMEOUT = config("SERIAL_TIMEOUT", default=1, cast=int)
 
 
 if __name__ == '__main__':
-    serial_port = serial.Serial('/dev/ttyUSB0')
+    serial_port = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=SERIAL_TIMEOUT)
 
     while True:
         line = serial_port.readline()
