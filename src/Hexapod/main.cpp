acee6fa877c7c333cde260a315f76b0512a3f784
h3nnn4n <git@renan.digital>
2023-06-25 12:09:47 -0300

    Wiring 6 legs with idenpendent IK
    ie each leg is running its own IK calculations, instead of the previous
    version where IK was solved for one leg and copied to all others.
    Visually the robot does the same things, but this proves that the
    hardware is good enough to run my lazy code in "real time" for all 18
    servos.

--- src/Hexapod/main.cpp
+++ src/Hexapod/main.cpp
@@ -16,111 +16,141 @@
 #include <VectorDatatype.h>
 #include <Wire.h>
 
-BlinkenLights blinkenlights = BlinkenLights();
-
-PWMServoDriver pca = PWMServoDriver(0x40);
-
-Joint tibia = Joint(&pca, 6);
-Joint femur = Joint(&pca, 5);
-Joint coxa  = Joint(&pca, 4);
-
-Leg leg = Leg(&coxa, &femur, &tibia);
-
-char buffer[400];
-char f_buffer[10];
-
-vec3_t positions[] = {
-    {0, 290, 0}, {0, 250, 0}, {0, 250, 50}, {0, 250, -50}, {0, 250, 0},
-};
-
-uint16_t current_position_i = 0;
-uint16_t target_position_i  = 1;
+const size_t buffer_size = 256;
+char         buffer[buffer_size];
+char         f_buffer[10];
 
 unsigned long long last_time = 0;
 unsigned long long now       = 0;
 
-int32_t  timer                  = 0;
-uint32_t time_between_positions = 2000;
+const float coxa_start_angle  = 0.0f;
+const float femur_start_angle = 80.0f;
+const float tibia_start_angle = 170.0f;
+
+const int n_legs = 6;
+
+BlinkenLights blinkenlights = BlinkenLights();
+
+PWMServoDriver pca1 = PWMServoDriver(0x40);
+PWMServoDriver pca2 = PWMServoDriver(0x41);
+
+Joint coxa1  = Joint(&pca1, 0);
+Joint femur1 = Joint(&pca1, 1);
+Joint tibia1 = Joint(&pca1, 2);
+
+Joint coxa2  = Joint(&pca1, 4);
+Joint femur2 = Joint(&pca1, 5);
+Joint tibia2 = Joint(&pca1, 6);
+
+Joint coxa3  = Joint(&pca1, 8);
+Joint femur3 = Joint(&pca1, 9);
+Joint tibia3 = Joint(&pca1, 10);
+
+Joint coxa4  = Joint(&pca2, 0);
+Joint femur4 = Joint(&pca2, 1);
+Joint tibia4 = Joint(&pca2, 2);
+
+Joint coxa5  = Joint(&pca2, 4);
+Joint femur5 = Joint(&pca2, 5);
+Joint tibia5 = Joint(&pca2, 6);
+
+Joint coxa6  = Joint(&pca2, 8);
+Joint femur6 = Joint(&pca2, 9);
+Joint tibia6 = Joint(&pca2, 10);
+
+Leg leg1 = Leg(&coxa1, &femur1, &tibia1);
+Leg leg2 = Leg(&coxa2, &femur2, &tibia2);
+Leg leg3 = Leg(&coxa3, &femur3, &tibia3);
+Leg leg4 = Leg(&coxa4, &femur4, &tibia4);
+Leg leg5 = Leg(&coxa5, &femur5, &tibia5);
+Leg leg6 = Leg(&coxa6, &femur6, &tibia6);
+
+Leg *legs[n_legs] = {
+    &leg1, &leg2, &leg3, &leg4, &leg5, &leg6,
+};
+
+vec3_t target_position;
 
 void setup() {
     Serial.begin(115200);
-    Serial.println("starting");
+    Serial.println("starting...");
 
     blinkenlights.init();
     blinkenlights.set_update_interval(50);
 
-    pca.begin();
-    pca.setPWMFreq(60);
+    pca1.begin();
+    pca1.setPWMFreq(60);
 
-    coxa.set_angle_range(-90.0f, 90.0f);
-    femur.set_angle_range(-90.0f, 90.0f);
-    tibia.set_angle_range(0.0f, 180.0f);
+    pca2.begin();
+    pca2.setPWMFreq(60);
 
-    leg.init();
-    leg.enable_servos();
-    leg.set_joint_angles(0, 0, 0);
-    leg.set_target_foot_position(positions[0]);
+    coxa1.set_angle_range(-90.0f, 90.0f);
+    femur1.set_angle_range(-90.0f, 90.0f);
+    tibia1.set_angle_range(0.0f, 180.0f);
 
-    now               = millis();
-    last_time         = millis();
-    timer             = time_between_positions;
-    target_position_i = current_position_i + 1;
+    coxa2.set_angle_range(-90.0f, 90.0f);
+    femur2.set_angle_range(-90.0f, 90.0f);
+    tibia2.set_angle_range(0.0f, 180.0f);
+
+    coxa3.set_angle_range(-90.0f, 90.0f);
+    femur3.set_angle_range(-90.0f, 90.0f);
+    tibia3.set_angle_range(0.0f, 180.0f);
+
+    coxa4.set_angle_range(-90.0f, 90.0f);
+    femur4.set_angle_range(-90.0f, 90.0f);
+    tibia4.set_angle_range(0.0f, 180.0f);
+
+    coxa5.set_angle_range(-90.0f, 90.0f);
+    femur5.set_angle_range(-90.0f, 90.0f);
+    tibia5.set_angle_range(0.0f, 180.0f);
+
+    coxa6.set_angle_range(-90.0f, 90.0f);
+    femur6.set_angle_range(-90.0f, 90.0f);
+    tibia6.set_angle_range(0.0f, 180.0f);
+
+    for (auto leg : legs) {
+        leg->init();
+        // leg->set_tolerance(2.5f);
+        leg->set_tolerance(1.0f);
+        leg->set_joint_angles(coxa_start_angle, femur_start_angle, tibia_start_angle);
+        leg->enable_servos();
+        leg->update();
+    }
+
+    now       = millis();
+    last_time = millis();
 }
 
 void loop() {
     blinkenlights.update();
 
-    now        = millis();
-    auto delta = now - last_time;
-    last_time  = now;
+    now         = millis();
+    float delta = (now - last_time) / 1000.0f;
+    last_time   = now;
 
-    timer -= delta;
+    float f       = millis() / 1000.0f;
+    float offset1 = sin(f) * 50.0f;
+    float offset2 = cos(f) * 25.0f * 0.0;
 
-    // snprintf(buffer, sizeof(buffer), "timer: %4d  delta: %4d  now: %ld  last_time:  %ld", timer, delta, now,
-    // last_time); Serial.println(buffer);
+    target_position = vec3_t(0.0f + offset2, 70.0f, -140.0f + offset1);
 
-    if (timer <= 0) {
-        Serial.println("\nmoving to new position");
-
-        timer = time_between_positions;
-
-        current_position_i++;
-
-        if (current_position_i >= sizeof(positions) / sizeof(vec3_t)) {
-            current_position_i = 0;
-        }
-
-        target_position_i = current_position_i + 1;
-
-        if (target_position_i >= sizeof(positions) / sizeof(vec3_t)) {
-            target_position_i = 0;
-        }
+    for (auto leg : legs) {
+        leg->set_target_foot_position(target_position);
+        leg->update();
     }
 
-    float p = (float)timer / (float)time_between_positions;
-    p       = 1.0f - p;
-
-    vec3_t target_position = vec3_lerp(positions[current_position_i], positions[target_position_i], p);
-
-    leg.set_target_foot_position(target_position);
-    leg.update();
-
-    snprintf(buffer, sizeof(buffer), "timer: %4d  ", timer);
+    snprintf(buffer, buffer_size, "dt:%4d ms", uint16_t(delta * 1000.0f));
     Serial.print(buffer);
-    Serial.print("  ");
-    Serial.print("p: ");
-    Serial.print(p);
-    Serial.print("  ");
-    Serial.print(current_position_i);
-    Serial.print("/");
-    Serial.print(target_position_i);
+    Serial.print(" ");
     Serial.print("  |  current: ");
-    serial_print_vec3(leg.get_current_position());
+    serial_print_vec3(leg1.get_current_position());
     Serial.print("  |  target: ");
-    serial_print_vec3(leg.get_target_position());
+    serial_print_vec3(leg1.get_target_position());
     Serial.print("  |  angles: ");
-    serial_print_vec3(leg.get_current_angles());
+    serial_print_vec3(leg1.get_current_angles());
     Serial.print("  |  error: ");
-    Serial.print(leg.get_error());
+    Serial.print(leg1.get_error());
+    Serial.print("  |  reach: ");
+    Serial.print(leg1.get_reach());
     Serial.println();
 }
