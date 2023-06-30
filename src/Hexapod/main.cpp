c798eca066affc6fac0a1f0d1c275358d673184c
h3nnn4n <git@renan.digital>
2023-06-30 16:43:00 -0300

    Revert "Remove hardcoded leg movement on boot"
    This reverts commit 430472cccada99040965ac89331168d1b60a6fc2.
    
    that was a bad idea. it just moved to 0, 0, 0 instead, which is way
    worse

--- src/Hexapod/main.cpp
+++ src/Hexapod/main.cpp
@@ -132,6 +132,7 @@ void setup() {
     for (auto leg : legs) {
         leg->init();
         leg->set_tolerance(1.0f);
+        leg->set_joint_angles(coxa_start_angle, femur_start_angle, tibia_start_angle);
         leg->disable_servos();
         leg->update();
     }
